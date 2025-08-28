893b699d1237fad682acf5b06eb04f29e9d79129
Reihaneh0-0 <reihan.38dt@gmail.com>
2025-08-28 12:00:07 +0330

    updating app.js

--- app.js
+++ app.js
@@ -1,2 +1,15 @@
 import dotenv from "dotenv"
+import TelegramBot from "node-telegram-bot-api"
 dotenv.config()
+const TOKEN = process.env.BOT_TOKEN;
+
+const bot = new TelegramBot (TOKEN, {
+    baseApiUrl: "https://tapi.bale.ai",
+    polling: true,
+});
+
+bot.on('message', (msg) => {
+  const chatId = msg.chat.id;
+  bot.sendMessage(chatId, msg.text)
+
+})
